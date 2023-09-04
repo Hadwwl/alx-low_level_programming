d2bad74fac70a7c92a2721deb30eceae531cac7c
hadeel <ddjobs2021@gmail.com>
2023-09-03 21:54:17 -0700

    argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - entry point
  * @argc: number of command line arguments
  * @argv: arry containing command line arguments
- * Return: 0 or 1
+ * Return: 0 success
  */
 int main(int argc, char *argv[])
 {
 	int sum = 0;
+	char *c;
 
-	if (argc == 1)
+	while (--argc)
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (int i = 1 ; i < argc ; i++)
-	{
-		int num = 0;
-		int j = 0;
-
-		while (argv[i][j] != '\0')
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			j++;
-		}
-		num = atoi(argv[i]);
-		sum += num;
+		for (c = argv[argc] ; *c ; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
 	return (0);
