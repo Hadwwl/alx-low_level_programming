926738a85cccf6bbc1a36b10eff8881141084273
hadeel <ddjobs2021@gmail.com>
2023-09-25 10:12:36 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,8 +19,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0 ; i < index ; i++)
 	{
 		if (love == NULL)
-			return NULL;
-		
+			return (NULL);
+
 		love = love->next;
 	}
 
