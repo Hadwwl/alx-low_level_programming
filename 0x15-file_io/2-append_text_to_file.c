01c040816b44cf9160f332dfdb6b12e8a9d0bcc8
hadeel <ddjobs2021@gmail.com>
2023-10-02 19:15:08 -0700

    File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: points to name of the file
+ * @text_content: points to NULL terminated string to add at the end of the file
+ * Return: 1 on success & -1 on failure or no file or content
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int openlove, writelove, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (length = 0 ; text_content[length];)
+			length++;
+	}
+
+	openlove = open(filename, O_WRONLY | O_APPEND);
+	writelove = write(openlove, text_content, length);
+
+	if (openlove == -1 || writelove == -1)
+	{
+		return (-1);
+	}
+
+	close(openlove);
+
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_file - creates 1024 bytes for buffer
+ * @file: name of file tgat stores buffer chars
+ * Return: pointer points to new buffer
+ */
+char *create_buffer(char *file)
+{
+	char *lovebuffer;
+
+	lovebuffer = malloc(sizeof(char) * 1024);
+
+	if (lovebuffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (lovebuffer);
+}
+
+/**
+ * close_file - close fd
+ * @fd: file discriptor to be closed
+ */
+void close_file(int fd)
+{
+	int closelove;
+
+	closelove = close(fd);
+
+	if (closelove == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - function to copy file content to other file
+ * @argv: arrays of pointers to arguments
+ * @argc: number of arguments
+ * @Return: 0 success
+ * 97 if arguments incorrect
+ * 99 if filefrom not exist or cann't read
+ * 100 if cannot close fd
+ */
+int main(int argc, char *argv[])
+{
+	int fromlove, tolove, readlove, writelove;
+	char *lovebuffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n");
+			exit(97);
+	}
+
+	lovebuffer = create_buffer(argv[2]);
+	fromlove = open(argv[1], O_RDONLY);
+	readlove = read(fromlove, lovebuffer, 1024);
+	tolove = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do{
+		if (fromlove == -1 || readlove == -1)
+		{
+			dprinf(STDERR_FILENO,
+				"
