98711af22755b2f6a3d85d21fc660bf46f764378
Hadeel <ddjobs2021@gmail.com>
2023-10-30 07:00:38 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,9 @@ int create_file(const char *filename, char *text_content)
 	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	writelov = write(file_desc, text_content, length);
 
+	if (text_content == NULL)
+		text_content = " ";
+
 	if (file_desc == -1 || writelov == -1)
 		return (-1);
 
