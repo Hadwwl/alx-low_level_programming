068522b6119968be692aaa99dab8e469f66efb7b
Hadwwl <ddjobs2021@gmail.com>
2023-12-08 18:57:38 -0800

    double

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-	for (; index != 0 ; index --)
+	for (; index != 0 ; index--)
 	{
 		if (temp == NULL)
 		{
